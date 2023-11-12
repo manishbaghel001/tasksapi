6773280b242b1dbd6cc3c397657d5592dedf0c71
Manish <manish20181998@gmail.com>
2023-11-12 17:20:27 +0530

    sadf

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use('/api/tasks', tasks);
 app.use('/api/todos', todos);
 app.use('/api/mode', mode);
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('Server listening on port ' + port);
+    console.log('Server listening on port 3000');
 });
